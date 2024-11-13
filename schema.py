c0fe1269cb2e10ca20fecae9ac1a09337ea33093
fatfoxmushrooms <fatfoxmushrooms@gmail.com>
2024-11-13 19:58:10 +0500

    Update schema.py

--- schema.py
+++ schema.py
@@ -3,5 +3,5 @@ required_columns = [
     "Order number", "Date created", "Total order quantity", "Contact email", "Note from customer",
     "Item", "Variant", "SKU", "Qty", "Quantity refunded", "Price", "Weight", "Custom text",
     "Deposit amount", "Delivery method", "Recipient name", "Delivery country", "Delivery city", 
-    "Delivery address", "Delivery zip/postal code"
+    "Delivery address", "Delivery zip/postal code", "Recipient Full Name"
 ]
